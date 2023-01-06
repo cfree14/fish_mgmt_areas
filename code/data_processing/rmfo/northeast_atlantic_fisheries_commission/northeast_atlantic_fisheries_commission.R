ea2eb51cc5477192feac7312374c1f22d38e2041
Allie Caughman <acaughman@ucsb.edu>
2023-01-05 16:10:15 -0800

    update path

--- code/data_processing/rmfo/northeast_atlantic_fisheries_commission/northeast_atlantic_fisheries_commission.R
+++ code/data_processing/rmfo/northeast_atlantic_fisheries_commission/northeast_atlantic_fisheries_commission.R
@@ -4,13 +4,15 @@ library(sf)
 # Setup
 ################################################################################
 
+# https://www.neafc.org/managing_fisheries/measures/ra_map
+
 # Directories
 
 basedir <- "G:/My Drive/projects/mgmt_area_database"
 setwd(basedir)
 
 # Read data
-data_orig <- st_read(file.path(basedir, "raw", "rmfo", "XXX", "XXX.shp"))
+data_orig <- st_read(file.path(basedir, "raw", "rmfo", "northeast_atlantic_fisheries_commission", "NEAFC_map.KML"))
 
 # Coordinate system
 wgs84 <- sf::st_crs("+proj=longlat +datum=WGS84")
@@ -22,37 +24,33 @@ wgs84 <- sf::st_crs("+proj=longlat +datum=WGS84")
 
 data <- data_orig %>%
   # Reproject
-  sf::st_transform(wgs84) %>%
+  sf::st_transform(wgs84)
+
+data <- data %>%
   # rename to geom
   rename(geom = geometry) %>% # required
+  select(-Description) %>%
   # add columns
   mutate(
-    Owner_name_english = "", # required
-    Owner_name_localized = "",
-    Owner_code = "", # required
-    Owner_code_localized = "",
-    Owner_code_official = "", # required
-    Owner_multinational = "", # required
-    Owner_country = "",
-    System_name_english = "", # required
-    System_name_localized = "",
-    System_additional_descriptor = "",
+    Owner_name_english = "North-East Atlantic Fisheries Commision", # required
+    Owner_code = "NEAFC", # required
+    Owner_code_official = 1, # required
+    Owner_multinational = 1, # required
+    System_name_english = "NEAFC Regulatory Areas", # required
     System_code = "", # required
-    System_code_localized = "",
-    System_code_official = "", # required
-    System_code_disambiguation = "",
+    System_code_localized = "NEAFCRA",
+    System_code_official = 0, # required
     System_multispecies = "",
     System_species_description = "",
     System_source = "", # required
-    System_source_date = "", # required
+    System_source_date = "2011-01-01", # required
     System_shape_file = "",
     System_georef_code = "",
-    System_license_terms = "", # required
+    System_license_terms = "Not explicit/unknown", # required
     System_lineage = "", # required
-    System_comment = "",
     System_type = "", # required
     System_type_code = "",
-    System_category = "", # required
+    System_category = "Regulatory Area", # required
     Area_systematic_name_english = "", # required
     Area_systematic_name_localized = "",
     Area_descriptive_name = "",
@@ -67,4 +65,4 @@ data <- data_orig %>%
 # Export data
 ################################################################################
 
-saveRDS(data, file = here::here("processed", "rmfo", "XXX", "XXX.Rds"))
+saveRDS(data, file = here::here("processed", "rmfo", "northeast_atlantic_fisheries_commission", "northeast_atlantic_fisheries_commission.Rds"))
