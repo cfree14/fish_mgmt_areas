e0ac0e7d949471f4b496f485f177f98c1c9d0ad3
Allie Caughman <acaughman@ucsb.edu>
2023-01-24 20:02:38 -0800

    switch mislabeled columns

--- code/data_processing/rmfo/commission_conservation_antartic_marine_living_resources/commission_conservation_antartic_marine_living_resources.R
+++ code/data_processing/rmfo/commission_conservation_antartic_marine_living_resources/commission_conservation_antartic_marine_living_resources.R
@@ -40,8 +40,8 @@ data <- data %>%
     Owner_code_official = "1", # required
     Owner_multinational = "1", # required
     System_name_english = "CCAMLR Area of Competence", # required
-    System_code = "0", # required
-    System_code_official = "CCAMLRAoC", # required
+    System_code = "CCAMLRAoC", # required
+    System_code_official = "0", # required
     System_multispecies = "1",
     System_source = "Food and Agriculture Organization (FAO). (30 Jan 2020). Geographic Area of Competence of Commission for the Conservation of Antarctic Marine Living Resources (CCAMLR). data.apps.fao.org. Retrieved 17 Jan 2023: https://data.apps.fao.org/map/catalog/srv/eng/catalog.search#/metadata/fao-rfb-map-ccamlr", # required
     System_source_date = "2020-01-30", # required
