49259223c0cfa01a85e9fe8fd1795db278630943
Allie Caughman <acaughman@ucsb.edu>
2023-04-14 18:26:46 -0700

    initial commit

---
+++ code/data_processing/country/mexico/mexico_13.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(sf)
+
+# Setup
+################################################################################
+
+# Directories
+
+basedir <- "G:/My Drive/projects/mgmt_area_database_ac"
+setwd(basedir)
+
+# Coordinate system
+wgs84 <- sf::st_crs("+proj=longlat +datum=WGS84")
+
+# Read data
+data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "shapefiles", ".shp")) %>%
+  sf::st_transform(wgs84)
+
+# Format data
+################################################################################
+
+# Format data
+
+data <- data_orig %>%
+  # rename to geom
+  rename(geom = geometry)
+
+data = data %>% # required
+  # add columns
+  mutate(
+    Owner_name_english = "National Institute of Fisheries and Aquaculture", # required
+    Owner_name_localized = "Instituto Nacional de Pesca y Acuacultura", # preferred if no official translation unless not roman alphabet
+    Owner_code = "INAPESCA", # required
+    Owner_code_localized = "INAPESCA", # preferred if no official translation
+    Owner_code_official = "1", # required
+    Owner_multinational = "0", # required
+    Owner_country = "mex",
+    System_name_english = "Fisheries Management Plan of ", # required
+    System_name_localized = "Plan de Manejo Pesquero ",
+    System_code = "FMP", # required
+    System_code_localized = "PMP",
+    System_code_official = "0", # required
+    System_multispecies = "1",
+    System_species_description = paste(""),
+    System_source = "", # required
+    System_source_date = "", # required
+    System_shape_file = ".shp",
+    System_license_terms = "Public Domain", # required
+    System_lineage = "Digitized by Alicia Caughman in QGIS on April XX 2023", # required
+    System_type = "", # required
+    System_category = "", # required
+    Area_systematic_name_english = "", # required
+    Area_descriptive_name = "",
+    Area_code_official = "", # required
+    Created_by = "Alicia Caughman / acaughman@ucsb.edu",
+    Created_on = Sys.Date()
+  )
+
+# Export data
+################################################################################
+
+saveRDS(data, file = file.path(basedir, "processed", "country", "mexico", "mexico_13.Rds"))
