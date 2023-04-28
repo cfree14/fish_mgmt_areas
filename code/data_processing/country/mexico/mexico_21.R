d410f631ce575f5f55c3d39c389e9e999e979d07
Allie Caughman <acaughman@ucsb.edu>
2023-04-28 14:26:17 -0700

    finish mexico

--- code/data_processing/country/mexico/mexico_21.R
+++ code/data_processing/country/mexico/mexico_21.R
@@ -13,7 +13,7 @@ setwd(basedir)
 wgs84 <- sf::st_crs("+proj=longlat +datum=WGS84")
 
 # Read data
-data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "shapefiles", ".shp")) %>%
+data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "pulpo_2.shp")) %>%
   sf::st_transform(wgs84)
 
 # Format data
@@ -23,7 +23,8 @@ data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "shapefiles"
 
 data <- data_orig %>%
   # rename to geom
-  rename(geom = geometry)
+  rename(geom = geometry) %>%
+  mutate(System_georef_code = "Area_code")
 
 data = data %>% # required
   # add columns
@@ -35,23 +36,23 @@ data = data %>% # required
     Owner_code_official = "1", # required
     Owner_multinational = "0", # required
     Owner_country = "mex",
-    System_name_english = "Fisheries Management Plan of ", # required
-    System_name_localized = "Plan de Manejo Pesquero ",
+    System_name_english = "Fisheries Management Plan for the Eastern Coast of Baja California", # required
+    System_name_localized = "Plan de Manejo Pesquero de la Costa Oriental de Baja California",
     System_code = "FMP", # required
     System_code_localized = "PMP",
     System_code_official = "0", # required
     System_multispecies = "1",
-    System_species_description = paste(""),
-    System_source = "", # required
-    System_source_date = "", # required
-    System_shape_file = ".shp",
+    System_species_description = paste("Octopus bimaculatus and Octopus hubbsorum"),
+    System_source = "Instituto Nacional de Pesca y Acuacultura. (15 Feb 2022). Plan de Manejo Pesquero de la Costa Oriental de Baja California. www.gob.mx/inapesca. Retrieved 14 March 2023: https://www.gob.mx/inapesca/documentos/97664", # required
+    System_source_date = "2022-02-15", # required
+    System_shape_file = "pulpo_2.shp",
     System_license_terms = "Public Domain", # required
-    System_lineage = "Digitized by Alicia Caughman in QGIS on April XX 2023", # required
-    System_type = "", # required
-    System_category = "", # required
-    Area_systematic_name_english = "", # required
-    Area_descriptive_name = "",
-    Area_code_official = "", # required
+    System_lineage = "Digitized by Alicia Caughman in QGIS on April 12 2023", # required
+    System_type = "Eastern Coast of Baja California Octopus Management Area", # required
+    System_category = "Management Area", # required
+    Area_systematic_name_english = "Eastern Coast of Baja California", # required
+    Area_descriptive_name = "la Costa Oriental de Baja California",
+    Area_code_official = "0", # required
     Created_by = "Alicia Caughman / acaughman@ucsb.edu",
     Created_on = Sys.Date()
   )
--- code/data_processing/country/mexico/mexico_22.R
+++ code/data_processing/country/mexico/mexico_22.R
@@ -13,7 +13,7 @@ setwd(basedir)
 wgs84 <- sf::st_crs("+proj=longlat +datum=WGS84")
 
 # Read data
-data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "shapefiles", ".shp")) %>%
+data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "pulpo_gm_y_mc.shp")) %>%
   sf::st_transform(wgs84)
 
 # Format data
@@ -23,7 +23,8 @@ data_orig <- st_read(file.path(basedir, "raw", "country", "mexico", "shapefiles"
 
 data <- data_orig %>%
   # rename to geom
-  rename(geom = geometry)
+  rename(geom = geometry) %>%
+  mutate(System_georef_code = "Area_code")
 
 data = data %>% # required
   # add columns
@@ -35,23 +36,23 @@ data = data %>% # required
     Owner_code_official = "1", # required
     Owner_multinational = "0", # required
     Owner_country = "mex",
-    System_name_english = "Fisheries Management Plan of ", # required
-    System_name_localized = "Plan de Manejo Pesquero ",
+    System_name_english = "Octopus Fishery Management Plan in the Gulf of Mexico and Caribbean Sea", # required
+    System_name_localized = "Plan de Manejo Pesquero de Pulpo en el Golfo de México y Mar Caribe",
     System_code = "FMP", # required
     System_code_localized = "PMP",
     System_code_official = "0", # required
     System_multispecies = "1",
-    System_species_description = paste(""),
-    System_source = "", # required
-    System_source_date = "", # required
-    System_shape_file = ".shp",
+    System_species_description = paste("O. maya and O. vulgaris"),
+    System_source = "Instituto Nacional de Pesca y Acuacultura. (28 March 2014). Plan de Manejo Pesquero de Pulpo en el Golfo de México y Mar Caribe. www.gob.mx/inapesca. Retrieved 14 March 2023: https://www.gob.mx/inapesca/documentos/plan-de-manejo-pesquero-de-pulpo-en-el-golfo-de-mexico-y-mar-caribe", # required
+    System_source_date = "2014-03-28", # required
+    System_shape_file = "pulpo_gm_y_mc.shp",
     System_license_terms = "Public Domain", # required
-    System_lineage = "Digitized by Alicia Caughman in QGIS on April XX 2023", # required
-    System_type = "", # required
-    System_category = "", # required
-    Area_systematic_name_english = "", # required
-    Area_descriptive_name = "",
-    Area_code_official = "", # required
+    System_lineage = "Digitized by Alicia Caughman in QGIS on April 14 2023", # required
+    System_type = "Octopus in the Gulf of Mexico and Caribbean Sea Management Area", # required
+    System_category = "Management Area", # required
+    Area_systematic_name_english = "Octopus Fishery in the Gulf of Mexico and Caribbean Sea", # required
+    Area_descriptive_name = "Pulpo en el Golfo de México y Mar Caribe",
+    Area_code_official = "0", # required
     Created_by = "Alicia Caughman / acaughman@ucsb.edu",
     Created_on = Sys.Date()
   )
